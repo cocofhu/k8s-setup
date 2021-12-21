ef1a86bef623853799aa625f0005fa7a4f530217
cocofhu <43171452+cocofhu@users.noreply.github.com>
2021-12-22 03:09:13 +0800

    Update 1.18.2.sh

--- 1.18.2.sh
+++ 1.18.2.sh
@@ -31,7 +31,7 @@ cat <<EOF >  /etc/sysctl.d/k8s.conf
 net.bridge.bridge-nf-call-ip6tables = 1
 net.bridge.bridge-nf-call-iptables = 1
 EOF
-
+echo "1" >/proc/sys/net/ipv4/ip_forward
 sysctl --system
 
 systemctl stop firewalld.service
